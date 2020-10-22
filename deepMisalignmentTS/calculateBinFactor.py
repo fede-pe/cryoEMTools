93ef933cc78e65c98c20daf6a6ae74180c48bf86
fede-pe <fepe.ig@gmail.com>
2020-10-22 11:54:22 +0200

    program working

--- deepMisalignmentTS/calculateBinFactor.py
+++ deepMisalignmentTS/calculateBinFactor.py
@@ -1,16 +1,25 @@
+import sys
+
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print("Usage: python calculateBinFactor.py <beadSize> <tomoBoxSize>\n"
               "<beadSize>:  Size in Angstroms (A) of the gold beads to be picked in the tomogram. \n"
-              "<pathAngleFile>: Path to file containing the tilt angles of the tilt-series. /n")
+              "<tomoBoxSize>: Path to file containing the tilt angles of the tilt-series. /n")
         exit()
 
     targetBeadPixSize = 10
-    inputBeadRealSize = sys.argv[1]
-    inputTomoBoxSize = sys.argv[2]
+    inputBeadRealSize = float(sys.argv[1])
+    inputTomoBoxSize = float(sys.argv[2])
 
     # Calculate the size of the beads from the original tomogram in pixels
     inputBeadPixSize = inputBeadRealSize / inputTomoBoxSize
+
+    # Calculate the binning factor to match the pixel size of the beads in the tomogram with target
+    binFactor = targetBeadPixSize / inputBeadPixSize
+
+    print("\n"
+          "\t Binning factor: %.4f \n" % binFactor,
+          "\t Target bead pixel size: %d pixels\n" % targetBeadPixSize)
 
 
 
