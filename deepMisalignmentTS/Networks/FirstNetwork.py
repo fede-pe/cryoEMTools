66c71a914843f10133be3d745835281780e3c8cb
fede-pe <fepe.ig@gmail.com>
2021-04-21 11:33:45 +0200

    change network structure I

--- deepMisalignmentTS/Networks/FirstNetwork.py
+++ deepMisalignmentTS/Networks/FirstNetwork.py
@@ -26,15 +26,15 @@ if __name__ == "__main__":
     def constructModel():
         inputLayer = Input(shape=(32, 32, 32, 1), name="input")
 
-        L = Conv3D(filters=16, kernel_size=8, activation="relu")(inputLayer)
+        L = Conv3D(filters=8, kernel_size=3, activation="relu")(inputLayer)
         L = MaxPool3D(pool_size=2)(L)
         L = BatchNormalization()(L)
 
-        L = Conv3D(filters=16, kernel_size=4, activation="relu")(L)
+        L = Conv3D(filters=16, kernel_size=3, activation="relu")(L)
         L = MaxPool3D(pool_size=2)(L)
         L = BatchNormalization()(L)
 
-        L = Conv3D(filters=16, kernel_size=4, activation="relu")(L)
+        L = Conv3D(filters=32, kernel_size=3, activation="relu")(L)
         L = MaxPool3D(pool_size=2)(L)
         L = BatchNormalization()(L)
 
