418b6d614b627e42d2301f831293c8515074124c
fede-pe <fepe.ig@gmail.com>
2021-08-26 11:37:34 +0200

    new method in utils

--- deepMisalignmentTS/Networks/utils.py
+++ deepMisalignmentTS/Networks/utils.py
@@ -1,6 +1,44 @@
 
 """ Module containing functions to parse and process datasets and results. """
 
+import numpy as np
+
+
+def normalizeInputDataStream(inputSubtomoStream):
+    """ Method to normalize the input subtomo data stream to """
+    std = inputSubtomoStream.std()
+    mean = inputSubtomoStream.mean()
+
+    normalizedInputDataStream = (inputSubtomoStream - mean) / std
+
+    return normalizedInputDataStream
+
+
+def statisticsFromInputDataStream(misalignmentInfoVector, variable, verbose=False):
+    """ This method calculates and outputs the statistics of the selected variable from the input information list.
+     Variable indicates the column number of the feature in the information matrix. """
+
+    mean = misalignmentInfoVector[:, variable].mean()
+    std = misalignmentInfoVector[:, variable].std()
+    median = np.median(misalignmentInfoVector[:, variable])
+    min = misalignmentInfoVector[:, variable].min()
+    max = misalignmentInfoVector[:, variable].max()
+
+    if verbose:
+        title = getTitleFromVariable(variable)
+        title = "------------------ " + title + " statistics:"
+
+        print(title)
+
+        print('Mean: ' + str(mean))
+        print('Std: ' + str(std))
+        print('Median: ' + str(median))
+        print('Min: ' + str(min))
+        print('Max: ' + str(max))
+        print('\n')
+
+    return mean, std, median, min, max
+
 
 def getTitleFromVariable(variable):
     """ This method returns the title (variable name) from the input variable identifier. """
