af485efaf49e444b81c7707bab21d58038d52940
fede-pe <fepe.ig@gmail.com>
2021-08-27 11:01:02 +0200

    fixes in scratch model

--- deepMisalignmentTS/Networks/CreateModel.py
+++ deepMisalignmentTS/Networks/CreateModel.py
@@ -22,27 +22,27 @@ def compileModel(model, learningRate):
 def scratchModel():
     inputLayer = Input(shape=(32, 32, 32, 1), name="input")
 
-    L = Conv3D(filters=8, kernel_size=3, activation="relu")(inputLayer)
+    L = Conv3D(filters=8, kernel_size=3, data_format="channels_last", padding="same", activation="relu")(inputLayer)
     L = MaxPool3D(pool_size=2)(L)
     L = BatchNormalization()(L)
 
-    L = Conv3D(filters=16, kernel_size=3, activation="relu")(L)
+    L = Conv3D(filters=16, kernel_size=3, data_format="channels_last", padding="same", activation="relu")(L)
     L = MaxPool3D(pool_size=2)(L)
     L = BatchNormalization()(L)
 
-    L = Conv3D(filters=32, kernel_size=3, activation="relu")(L)
+    L = Conv3D(filters=32, kernel_size=3, data_format="channels_last", padding="same", activation="relu")(L)
     L = MaxPool3D(pool_size=2)(L)
     L = BatchNormalization()(L)
 
-    L = Conv3D(filters=64, kernel_size=3, activation="relu")(L)
+    L = Conv3D(filters=64, kernel_size=3, data_format="channels_last", padding="same", activation="relu")(L)
     L = MaxPool3D(pool_size=2)(L)
     L = BatchNormalization()(L)
 
     L = Flatten()(L)
-    L = GlobalAveragePooling3D()(L)
-    L = Dense(units=512, activation="relu")(L)
+    # L = GlobalAveragePooling3D()(L)
+    L = Dense(units=256, activation="relu")(L)
     L = Dropout(0.2)(L)
 
-    L = Dense(units=6, name="output", activation="softmax")(L)
+    L = Dense(units=8, name="output", activation="softmax")(L)
 
     return Model(inputLayer, L, name="3dDNNmisali")
