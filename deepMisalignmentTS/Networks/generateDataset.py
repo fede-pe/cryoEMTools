36e11bd9462566576ed74d6087dfc52faddb8630
fede-pe <fepe.ig@gmail.com>
2021-10-11 14:43:25 +0200

    add mode to split output vectors

--- deepMisalignmentTS/Networks/generateDataset.py
+++ deepMisalignmentTS/Networks/generateDataset.py
@@ -99,15 +99,76 @@ def generateNetworkVectors():
         np.save(inputDataStreamPath, inputDataStream)
         np.save(misalignmentInfoPath, misalignmentInfoList)
 
+        print("Output subtomo vector saved at" + inputDataStreamPath)
+        print("Output misalignment info vector saved at" + misalignmentInfoPath)
+
+
+def generateNetworkVectorsSplit():
+    """ This method generates the vectors associated to the metadata files for posteriorly training and testing the
+    network. """
+
+    fileName = 'misalignmentMetadata.txt'
+    filePrefix = os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), '../trainingSet')
+    filePath = os.path.join(filePrefix, fileName)
+
+    with open(filePath) as f:
+        metadataLines = csv.DictReader(f, delimiter='\t')
+
+        # fieldNames = ['subTomoPath', 'alignmentToggle']
+
+        NdimAli = 0
+        NdimMisali = 0
+        subtomoPathListAli = []
+        subtomoPathListMisali = []
+
+        # Split subtomo paths between aligned and misaligned sets
+        for i, line in enumerate(metadataLines):
+            # Aligned
+            if int(line["alignmentToggle"]) == 1:
+                subtomoPathListAli.append(line["subTomoPath"])
+                NdimAli += 1
+
+            # Misaligned
+            elif int(line["alignmentToggle"]) == 0:
+                subtomoPathListMisali.append(line["subTomoPath"])
+                NdimMisali += 1
+
+        inputDataStreamAli = np.zeros((NdimAli, 32, 32, 32), dtype=np.float64)
+        inputDataStreamMisali = np.zeros((NdimMisali, 32, 32, 32), dtype=np.float64)
+
+        # Complete inputDataStreamAli and inputDataStreamMisali matrix (it is only possible to iterate over the
+        # csvReader once and it is necessary to know NdimAli and Misali a priori.
+        for i, subtomoPath in enumerate(subtomoPathListAli):
+            subtomoVol = xmipp.Image(subtomoPath).getData()
+            inputDataStreamAli[i, :, :, :] = subtomoVol
+
+        for i, subtomoPath in enumerate(subtomoPathListMisali):
+            subtomoVol = xmipp.Image(subtomoPath).getData()
+            inputDataStreamMisali[i, :, :, :] = subtomoVol
+
+        inputDataStreamAliPath = os.path.join(filePrefix, "inputDataStreamAli.npy")
+        inputDataStreamMisaliPath = os.path.join(filePrefix, "inputDataStreamMisali.npy")
+
+        np.save(inputDataStreamAliPath, inputDataStreamAli)
+        np.save(inputDataStreamMisaliPath, inputDataStreamMisali)
+
+        print("Output aligned subtomo vector saved at" + inputDataStreamAliPath)
+        print("Output misaligned subtomo vector saved at" + inputDataStreamMisaliPath)
+
 
 # ----------------------------------- Main ------------------------------------------------
 if __name__ == "__main__":
-
-    if len(sys.argv) == 1:
-        print("Preparing stack...")
+    if len(sys.argv) == 2 and (sys.argv[1] != "0" or sys.argv[1] != "1"):
         start_time = time()
 
-        generateNetworkVectors()
+        splitFlag = int(sys.argv[1])
+
+        if splitFlag == 0:
+            print("Preparing stack...")
+            generateNetworkVectors()
+        elif splitFlag == 1:
+            print("Preparing stack in split mode...")
+            generateNetworkVectorsSplit()
 
         elapsed_time = time() - start_time
         print("Time spent preparing the data: %0.10f seconds." % elapsed_time)
@@ -132,6 +193,8 @@ if __name__ == "__main__":
               "<pathPatternToSubtomoFiles>: Regex path to the subtomo volume files.\n"
               "<alignmentFlag 1/0>: Flag to set the imported subtomos as aligned (1) or misaligned (0). \n\n"
               ""
-              "Option 2: If no options are entered the program will create the output vectors for posteriorly train "
-              "and test the network:\n"
-              "python generateDataset.py")
+              "Option 2: Create the output vectors indicating if the misaligned and aligned subtomos are split in two "
+              "different vectors (1) or not (0). if not, a second vector containing the misalignment information would "
+              "be generated:\n"
+              "python generateDataset.py <splitVectorFlag 1/0>\n"
+              "<splitVectorFlag 1/0>: Flag to set if the output vectors must be split.")
