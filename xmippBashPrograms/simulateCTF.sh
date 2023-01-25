b584f50d30f3a2b8ddfc3bbd49239dc073ac08ad
fede-pe <fepe.ig@gmail.com>
2023-01-25 14:55:10 +0100

    make defocus random

--- xmippBashPrograms/simulateCTF.sh
+++ xmippBashPrograms/simulateCTF.sh
@@ -1,4 +1,5 @@
 for FILE in *.mrc;
-do echo "xmipp_transform_filter -i $FILE -o "${FILE%.mrc}_ctf.mrc" --fourier ctfdef 300.000000 2.700000 0.070000 20000.000000 --sampling 1.000000 -v 0";
-xmipp_transform_filter -i $FILE -o "${FILE%.mrc}_ctf.mrc" --fourier ctfdef 300.000000 2.700000 0.070000 20000.000000 --sampling 1.000000 -v 0;
+do DEFOCUS=$(shuf -i 4000-35000 -n1)
+echo "xmipp_transform_filter -i $FILE -o "${FILE%.mrc}_ctf.mrc" --fourier ctfdef 300.000000 2.700000 0.070000 $DEFOCUS --sampling 1.000000 -v 0";
+xmipp_transform_filter -i $FILE -o "${FILE%.mrc}_ctf.mrc" --fourier ctfdef 300.000000 2.700000 0.070000 $DEFOCUS --sampling 1.000000 -v 0;
 done;
