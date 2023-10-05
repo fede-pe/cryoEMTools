b91e5acfe23ec8a6b3572d2207b6db47fe2e421c
fede-pe <fepe.ig@gmail.com>
2023-10-05 16:14:09 +0200

    update number of epochs

--- deepMisalignmentHCF/Networks/trainDynamicModel.py
+++ deepMisalignmentHCF/Networks/trainDynamicModel.py
@@ -19,7 +19,7 @@ import utils
 # Module variables
 SUBTOMO_SIZE = 32  # Dimensions of the subtomos (cubic, SUBTOMO_SIZE x SUBTOMO_SIZE x SUBTOMO_SIZE shape)
 BATCH_SIZE = 64  # Number of boxes per batch
-EPOCHS = 2  # Number of epochs
+EPOCHS = 50  # Number of epochs
 LEARNING_RATE = 0.0001  # Learning rate
 TESTING_SPLIT = 0.15  # Ratio of data used for testing
 VALIDATION_SPLIT = 0.2  # Ratio of data used for validation
